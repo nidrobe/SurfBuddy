09af0bc63affbad7bc71b35bb664f1ca7d73ab48
Nico Drobe <nico.win777@gmail.com>
2025-04-13 12:19:08 +0200

    update tests and requirements

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,20 +1,18 @@
-from django.test import TestCase, Client
+from django.test import TestCase
 from django.urls import reverse
 import json
 from rest_framework import status
 from rest_framework.test import APIRequestFactory, APIClient
-from api.views import User  
-from api.serializers import UserSerializer
 
 class CreateUserTests(TestCase):
     def setUp(self):
-        self.client = APIClient() # Use APIClient for better API testing.
+        self.client = APIClient() 
         self.factory = APIRequestFactory()
 
     # Simulate a POST request to the /api/create_user/ endpoint with JSON data.
     def test_create_user(self):
         url = reverse('create_user')  
-    # Data for creating a user
+    # Providing data for creating a user
         data = {
                 "username": "testuser",
                 "password": "testpassword",
