a396c0d9ad9abb8854d5af804eee8bf0403eadbb
Nico Drobe <nico.win777@gmail.com>
2025-04-14 11:28:44 +0200

    add surfboard object for data serialization

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import Surfboard
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,3 +12,14 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+    
+class SurfboardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Surfboard
+        fields = '__all__'
+        extra_kwargs = {
+            'length_units': {'default': 'feet'},
+            'width_units': {'default': 'inches'},
+            'thickness_units': {'default': 'inches'},
+            'volume_units': {'default': 'liters'},
+        }
