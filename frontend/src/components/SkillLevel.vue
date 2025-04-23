2a7e6900c3de6b4d18e019619de46896b635ddea
Nico Drobe <nico.win777@gmail.com>
2025-04-23 21:45:41 +0200

    add content for skilllevel component

--- frontend/src/components/SkillLevel.vue
+++ frontend/src/components/SkillLevel.vue
@@ -1,7 +1,22 @@
 <template>
   <div class="skilllevel-text">
-    <p>Skill Level</p>
+    <div>
+      <span class="firstline">Which Surf</span>
+      <br>
+      <span class="secondline">level do I have?</span>
+      <br>
+      <span><i class="arrowdown"></i></span>
+    </div>
+
+    <div class="navigation">
+      <ul>
+        <li><a href="#beginner">Beginner</a></li>
+        <li><a href="#intermediate">Intermediate</a></li>
+        <li><a href="#advanced">Advanced</a></li>
+      </ul>
+    </div>
   </div>
+
 </template>
 
 <script>
@@ -18,5 +33,74 @@
     padding: 8px;
     margin: 8px;
     color: rgba(0,0,0,0.8);
+    text-align: center;
   }
+
+  .firstline {
+    font-family: "Open Sans", sans-serif;
+    display: inline-block;  
+    transform: rotate(-9deg) skewX(-6deg);
+    font-size: 1.66667rem;
+    text-transform: uppercase;
+    letter-spacing: .1rem;
+    overflow:auto;
+    color: #999;
+  }
+
+  .secondline {
+    font-family: "Open Sans",sans-serif;
+    display: inline-block;
+    transform: rotate(-9deg) skewX(-6deg);
+    font-size: 1.66667rem;
+    text-transform: uppercase;
+    letter-spacing: .1rem;
+    overflow: auto;
+    color: #32cdab;
+    position: relative;
+    top: -.83333rem;
+  }
+
+  .arrowdown {
+    border: solid #32cdab;
+    border-width: 0 3px 3px 0;
+    display: inline-block;
+    padding: 9px;
+    transform: rotate(45deg);
+    -webkit-transform: rotate(45deg);
+  }
+
+  .navigation {
+    font-family: "Open Sans",sans-serif;
+    padding: 10px;
+    color: #999;
+  }
+
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  display: flex;
+  justify-content: space-around;
+}
+
+li {
+  flex-grow: 1;
+}
+
+li a {
+  display: block;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  color: inherit; 
+}
+
+li a:hover {
+  color: #32cdab;
+  text-shadow: none;
+  background: none;
+}
+  
 </style>
