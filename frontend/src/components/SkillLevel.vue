ea636af161c35121649919a7175fe282b3308dd8
Nico Drobe <nico.win777@gmail.com>
2025-04-25 12:48:22 +0200

    add intermediate and advanced surf level content

--- frontend/src/components/SkillLevel.vue
+++ frontend/src/components/SkillLevel.vue
@@ -10,14 +10,15 @@
 
     <div class="navigation">
       <ul>
-        <li><a href="#beginner">Beginner</a></li>
-        <li><a href="#intermediate">Intermediate</a></li>
-        <li><a href="#advanced">Advanced</a></li>
+        <li><a href="#beginner" @click.prevent="selectedLevel = 'beginner'">Beginner</a></li>
+        <li><a href="#intermediate" @click.prevent="selectedLevel = 'intermediate'">Intermediate</a></li>
+        <li><a href="#advanced" @click.prevent="selectedLevel = 'advanced'">Advanced</a></li>
       </ul>
     </div>
 
     <div class="skillevel-description">
-      <div class="beginner-description">
+
+      <div class="beginner-description" v-if="selectedLevel === 'beginner'">
         <h3 class="beginner-description-header">White water and your first waves</h3>
         <br>
         <div class="beginner-description-content">
@@ -28,17 +29,52 @@
           </p>
         </div>
       </div>
-      <div class="beginner-image">
+      <div class="beginner-image" v-if="selectedLevel === 'beginner'">
         <img src="../assets/beginner.jpeg" alt="beginner surfers at the beach" width="200" height="200">
       </div>
+
+      <div class="intermediate-description" v-if="selectedLevel === 'intermediate'">
+        <h3 class="intermedaite-description-header">Takeoffs and turns</h3>
+        <br>
+        <div class="intermediate-description-content">
+          <p>
+            Intermediate surfers paddle on green, unbroken waves. They can takeoff cleanly and work straight on top and bottom turns.
+            Medium volume surfboards are for you. They are more playful and agile, perfect for improving your skills. 
+          </p>
+        </div>
+      </div>
+      <div class="intermediate-image" v-if="selectedLevel === 'intermediate'">
+        <img src="../assets/intermediate2.jpg" alt="intermediate surfer riding a wave" width="200" height="200">
+      </div>
+
+      <div class="advanced-description" v-if="selectedLevel === 'advanced'">
+        <h3 class="advanced-description-header">Cutbacks and style</h3>
+        <br>
+        <div class="advanced-description-content">
+          <p>
+            As an experienced surfer, you feel comfortable on any waves. Cutbacks, top to bottom turns and duck dives are no problem for you, 
+            your surfboard is always under control.
+            Depending on wave size and swell, you can choose a board that fits your surfing style.
+          </p>
+        </div>
+      </div>
+      <div class="advanced-image" v-if="selectedLevel === 'advanced'">
+        <img src="../assets/advanced1.jpg" alt="advanced surfer next to a barreling wave" width="200" height="200">
+      </div>
     </div>
+    
   </div>
 
 </template>
 
 <script>
   export default {
-    name: 'SkillLevel'
+    name: 'SkillLevel',
+    data() {
+      return {
+        selectedLevel: 'beginner' // Initial skill level
+      };
+    }
   }
 </script>
   
@@ -126,6 +162,8 @@ li a:hover {
     justify-content: center; /* Distribute items horizontal*/
   }
 
+  .advanced-description,
+  .intermediate-description,
   .beginner-description {
     flex: 1;                 /* Allow it to grow and take available space */
     padding-right: 1px;     /* Add some space between text and image */
@@ -134,6 +172,8 @@ li a:hover {
     line-height: 1.4;
   }
 
+  .advanced-image,
+  .intermediate-image,
   .beginner-image {
     flex: 1;                 /* Allow it to grow and take available space */
   }
